507da4fbdbf1d56820c7c8413adc484402bed0c2
paulbit3 <paultechnologie@gmail.com>
2019-03-25 16:02:49 -0500

    first commit

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from core.models import Movie
+
+admin.site.register(Movie)
+
+
---
+++ core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import  path
+
+from . import views
+
+app_name = 'core'
+urlpatterns = [
+    path('movies', views.MovieList.as_view(),
+         name='MovieList'),
+    path('movie/<int:pk>', views.MovieDetail.as_view(),
+         name='MovieDetail'),
+    path('movie/<int:movie_id>/vote', views.CreateVote.as_view(),
+         name='CreateVote'),
+    path('movie/<int:movie_id>/vote/<int:pk>', views.UpdateVote.as_view(),
+         name='UpdateVote'),
+    path('movie/<int:;ovie_id>/image/upload', views.MovieImageUpload.as_view(),
+         name='MovieImageUpload'),
+]
